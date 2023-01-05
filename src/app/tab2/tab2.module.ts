33981cd6a576214f56b0bafa891587e47ca7b486
Airam <airam.dev@gmail.com>
2023-01-05 20:26:36 +0000

    Converts currentLocation to an Observable and subscribes it from components, creates loading-screen component

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,14 +5,16 @@ import {FormsModule} from '@angular/forms';
 import {Tab2Page} from './tab2.page';
 import {Tab2PageRoutingModule} from './tab2-routing.module';
 import {MapModule} from '../components/map/map.module';
+import {LoadingScreenModule} from '../components/loading-screen/loading-screen.module';
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
-    Tab2PageRoutingModule,
-    MapModule
+    IonicModule,
+    LoadingScreenModule,
+    MapModule,
+    Tab2PageRoutingModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GeolocationService} from '../services/geolocation.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  isLocationLoaded: boolean = false;
+
+  constructor(
+    private geolocation: GeolocationService
+  ) {
+    this.geolocation.setCurrentPosition();
+  }
+
+  ngOnInit() {
+    this.geolocation.getCurrentPosition$().subscribe(position => {
+        position
+          ? this.isLocationLoaded = true
+          : this.isLocationLoaded = false
+      }
+    );
+  }
 
 }
